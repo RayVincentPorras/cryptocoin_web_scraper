a7f1b897834015abcf62bd70ef20a8a6b4bdd9a1
Ray Porras <rayporras@Rays-Mac-mini.local>
2018-01-23 22:17:21 -0800

    Added comments

--- cryptocoin_web_scraper.py
+++ cryptocoin_web_scraper.py
@@ -2,6 +2,7 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
+# Define function to get page html data and find the coin's current price
 def get_quote_price(url):
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, 'html.parser')
@@ -10,6 +11,7 @@ def get_quote_price(url):
     print(price)
 
 
+# List to store coin market pages
 marketPageList = ['https://coinmarketcap.com/currencies/ripple',
                   'https://coinmarketcap.com/currencies/iota',
                   'https://coinmarketcap.com/currencies/tron',
@@ -17,5 +19,6 @@ marketPageList = ['https://coinmarketcap.com/currencies/ripple',
                   'https://coinmarketcap.com/currencies/ethereum',
                   'https://coinmarketcap.com/currencies/litecoin']
 
+# For loop to iterate through the market page list calling the get quote function on each page
 for url in marketPageList:
     get_quote_price(url)
